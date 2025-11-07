61a02a9905e41cae9e948cc010dee9a3037ee728
Marcos4-C <cruzblo2@uwm.edu>
2025-11-07 00:39:14 -0600

    Add files via upload

---
+++ finaltruismvideo_CruzBloom/scripts.js
@@ -0,0 +1,43 @@
+// Select the video element and the text box
+const videoPlayer = document.getElementById("videoPlayer");
+const textBox = document.getElementById("textBox");
+
+// Array of video sources for toggling
+const videoSources = ["videos/truism_video.mp4",
+					  "videos/truism_video2.mp4",
+					  "videos/truism_video3.mp4",
+					  "videos/truism_video4.mp4",
+					  "videos/truism_video5.mp4"];
+// Current video index, starting with the first video
+let currentVideoIndex = 0;
+// Array to store the playback time for each video
+const videoTimes = [0, 0, 0, 0, 0];
+
+var isSoundEnabled = false;
+
+function getRandomIndex() {
+let newIndex;
+do {
+newIndex = Math.floor(Math.random() *
+videoSources.length);
+} while (newIndex === currentVideoIndex);
+return newIndex;
+}
+
+function toggleVideo() {
+	
+	if (isSoundEnabled === false) {
+videoPlayer.muted = false;
+isSoundEnabled = true;
+}
+	
+videoTimes[currentVideoIndex] = videoPlayer.currentTime;
+currentVideoIndex = getRandomIndex();
+	
+videoPlayer.src = videoSources[currentVideoIndex];
+videoPlayer.load();
+videoPlayer.currentTime = videoTimes[currentVideoIndex];
+videoPlayer.play();
+}
+// Add a click event to the text box to switch videos
+textBox.addEventListener("click", toggleVideo);
